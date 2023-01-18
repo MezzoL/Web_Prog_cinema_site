61593be313c5ff44ab38ccdabe87e81c67704ffb
MezzoL <102746853+MezzoL@users.noreply.github.com>
2023-01-18 17:09:15 +0100

    Third commit
    just frontend changes

--- includes/nav.php
+++ includes/nav.php
@@ -4,18 +4,18 @@
             <img class="nav-logo" src = "assets/logo.png"/>
         </a>
         <div class="links-right">
-            <a href="index.php">Movies</a>
+            <a class="glow-on-hover" href="index.php">Movies</a>
             <?php 
             if(!isset($_SESSION['email'])){ ?>
-                <a href="register.php">Register</a>
-                <a href="login.php">Log in</a>
+                <a class="glow-on-hover" href="register.php">Register</a>
+                <a class="glow-on-hover" href="login.php">Log in</a>
                 <?php }
             
             if(isset($_SESSION['email'])){ ?>
-                <a href="logout.php">Log out</a>
+                <a class="glow-on-hover" href="logout.php">Log out</a>
             <?php }
             ?>
-            <a href="moviebookingrequest.php">Request a movie</a>
+            <a class="glow-on-hover" href="moviebookingrequest.php">Request a movie</a>
         </div>
     </div>
 </nav>
